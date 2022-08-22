07ffdbbffa249029626534f3e2fe4c3c93753169
sreeramvamshi <108399924+sreeramvamshi@users.noreply.github.com>
2022-08-22 15:18:17 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i=0; 1<numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+alert("i got clicked ");
+    });
+}
